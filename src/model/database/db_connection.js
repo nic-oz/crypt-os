1c6c032f1078d7a266323b94443d69ecaca527cc
Nicos <nick.zonias@gmail.com>
2018-06-22 17:27:12 +0100

    modify database script

--- src/model/database/db_connection.js
+++ src/model/database/db_connection.js
@@ -2,7 +2,7 @@ const pgp = require('pg-promise')();
 const url = require('url');
 require('env2')('.env');
 
-let DB_URL = process.env.DB_URL;
+let DB_URL = process.env.DATABASE_URL;
 
 // if (process.env.NODE_ENV === 'test') {
 //     DB_URL = process.env.TEST_DB_URL;
@@ -14,13 +14,13 @@ const params = url.parse(DB_URL);
 const [username, password] = params.auth.split(':');
 
 const options = {
-    host: params.hostname,
-    port: params.port,
-    database: params.pathname.split('/')[1],
-    max: process.env.DB_MAX_CONNECTIONS || 2,
-    user: username,
-    password,
-    ssl: params.hostname !== 'localhost'
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
+  max: process.env.DB_MAX_CONNECTIONS || 2,
+  user: username,
+  password,
+  ssl: params.hostname !== 'localhost'
 };
 
 module.exports = pgp(options);
